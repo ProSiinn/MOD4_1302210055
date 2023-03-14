79db8d586396950fac7afc14816c075efaa7cc89
Stevent <fasyaraihan82@gmail.com>
2023-03-14 16:04:01 +0700

    commit pertama

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOD4_1302210055
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
